af435aea8c6227116a15c253de8dc8ee5ca17d8b
EdinaldoCodes <nedinaldo47@gmail.com>
2026-02-05 20:37:20 -0300

    Initial commit: Setup Hyprland completo

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+
+# overwrite greeting
+# Isso desativa o fastfetch e mensagens de boas-vindas
+function fish_greeting
+    # Deixar vazio intencionalmente
+end
